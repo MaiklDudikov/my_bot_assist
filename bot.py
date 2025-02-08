5dd53752d2d4c9f01e7984ca109b04e0fae6ccbc
MaiklDudikov <wmiwa87@gmail.com>
2025-02-08 16:39:36 +0200

    draft1

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет!')
+    await message.answer('Приве!')
 
 
 async def main():
